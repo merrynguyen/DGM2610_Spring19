43bec03b92ba8f872d27ede36629429fcddc36fb
Unknown <merryannnguyen@yahoo.com>
2019-02-03 11:48:16 -0700

    started level build out and game mechanics

---
+++ HoneyBadgerBee/Assets/Scripts/HoneyBadger.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoneyBadger : MonoBehaviour
+{
+
+
+
+    
+    private Boolean IsDead;
+    public float upForce = 300;
+    public Rigidbody rgbd;
+ 
+    public Vector3 Movement;
+    public int speed =10
+        ;
+    
+        void Start ()
+        {
+            rgbd= GetComponent<Rigidbody>();
+        }
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                rgbd.velocity = Vector2.zero;
+                rgbd.AddForce(new Vector2(100, upForce));
+               
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                Movement = rgbd.velocity;
+                Movement.x = Input.GetAxis("Horizontal") * speed;
+                rgbd.velocity = Movement;
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                Movement = rgbd.velocity;
+                Movement.x = Input.GetAxis("Horizontal") * speed;
+                rgbd.velocity = Movement;
+            }
+
+            if (Input.GetKeyUp(KeyCode.A))
+            {
+                Movement = rgbd.velocity;
+                Movement.x = 0;
+                rgbd.velocity = Movement;
+            }
+            
+            if (Input.GetKeyUp(KeyCode.D))
+            {
+                Movement = rgbd.velocity;
+                Movement.x = 0;
+                rgbd.velocity = Movement;
+            }
+        }
+    
+
+
+}
+
